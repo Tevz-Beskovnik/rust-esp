677c2dacf08f3afdddca0d2a123bc527dcf960a9
tevzi2 <tevz.beskovnik@gmail.com>
2024-04-14 20:28:07 +0200

    Commit files

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use esp_idf_svc::hal::delay::{self, Delay};
+
+fn main() {
+    // It is necessary to call this function once. Otherwise some patches to the runtime
+    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
+    esp_idf_svc::sys::link_patches();
+
+    // Bind the log crate to the ESP Logging facilities
+    esp_idf_svc::log::EspLogger::initialize_default();
+
+    log::info!("Hello, world!");
+    log::info!("Hello, world, again ahaha!");
+
+    let delay: Delay = Default::default();
+
+    loop {
+        delay.delay_us(10000);
+        log::info!("In loop!");
+    }
+}
